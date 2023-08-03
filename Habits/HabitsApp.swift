cdcdd7ee7fa61ebe5ac2c12f26a434b05699b4b9
antunigr <antuniribeiro@gmail.com>
2023-08-03 11:28:57 -0700

    Initial Commit

---
+++ Habits/HabitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitsApp.swift
+//  Habits
+//
+//  Created by antuni gt on 03/08/23.
+//
+
+import SwiftUI
+
+@main
+struct HabitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
